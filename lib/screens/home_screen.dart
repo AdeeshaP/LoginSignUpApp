f756eb18f8be21bdcd24f22713c86a88462d8460
Adeesha Perera <adeeshataxila@gmail.com>
2024-02-28 19:41:17 +0530

    login. signup and home screens created

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:login_register_app/screens/login_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, required this.name});
+
+  final String name;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber.shade400,
+        title: Text(
+          "Home",
+          style: TextStyle(
+            fontSize: 22,
+            color: Colors.black,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.logout),
+          onPressed: () {
+            FirebaseAuth.instance.signOut();
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (_) => LoginScreen()),
+              (route) => false,
+            );
+          },
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          widget.name,
+          style: TextStyle(
+            fontSize: 22,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
